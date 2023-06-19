4a4046482c8cb2041b429983758db5ffd749a87c
artaco <63841250+artaco@users.noreply.github.com>
2023-06-19 21:00:05 +0900

    お買い物リストの更新とログアウト時の画像を準備

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -4,7 +4,75 @@ import Image from "next/image";
 export default function Page() {
   return (
     <>
-      <p className={styles.title}>お気に入り</p>
+      <p className={styles.title}>買い物リスト</p>
+      <div className="flex justify-between mt-3 py-3 [border-bottom:1px_solid_lightgray]">
+        <p className="text-[18px]">じぶんメモ</p>
+        <p className="cursor-pointer">＋</p>
+      </div>
+      <div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">●</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">◯</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">●</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">●</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between mt-3 py-3 [border-bottom:1px_solid_lightgray]">
+        <p className="text-[18px]">グラタン</p>
+        <p className="cursor-pointer">︙</p>
+      </div>
+      <div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">●</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">◯</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">●</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+        <div className="flex py-2 [border-bottom:1px_solid_lightgray]">
+          <button className="p-1">●</button>
+          <p className="w-[85%] p-1">キャベツ</p>
+          <p className="p-1 cursor-pointer text-[red] hover:[text-decoration-line:underline]">
+            削除
+          </p>
+        </div>
+      </div>
     </>
   );
 }
