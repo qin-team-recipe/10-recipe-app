039e53bb969bb86f8e1d1e5a937126e0e8720a66
KohnoTaiyo <59274850+KohnoTaiyo@users.noreply.github.com>
2023-06-21 22:13:10 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,94 @@
 import { NextPage } from "next";
 
-const Page: NextPage = () => {
-  return <div className="text-tomato">トップページ</div>;
+import { ImageCarousel } from "@/components/ImageCarousel";
+import { ImageComponent } from "@/components/ImageComponent";
+import { ImageGrid } from "@/components/ImageGrid";
+
+type Mock = {
+  title: string;
+  description?: string;
+  favNum?: number;
+  nameLabel?: string;
+  src: string;
 };
 
-export default Page;
+const Home: NextPage = () => {
+  const mockData: Mock[] = [
+    {
+      title: "トマトとルッコラのマルゲリータトマトとルッコラのマルゲリータトマトとルッコラのマルゲリータ",
+      description: "トマトとルッコラのマルゲリータトマトとルッコラのマルゲリータトマトとルッコラのマルゲリータ",
+      favNum: 2000,
+      nameLabel: "山田シェフ",
+      src: "/images/sample_chef.jpg",
+    },
+    {
+      title: "トマトとルッコラ",
+      description: "トマトとルッコラのマルゲリータ",
+      nameLabel: "武田シェフ",
+      src: "/images/sample_chef.jpg",
+    },
+    ...[...Array(10)].map((_) => ({
+      title: "トマトとルッコラのマルゲリータトマトとルッコラのマルゲリータトマトとルッコラのマルゲリータ",
+      src: "/images/sample_chef.jpg",
+    })),
+  ];
+
+  return (
+    <>
+      <ImageCarousel>
+        {mockData.map((data, index) => (
+          <ImageComponent
+            key={`top-carousel-${index}`}
+            title={data.title}
+            alt={`${data.nameLabel || data.title}の画像`}
+            description={data.description}
+            nameLabel={data.nameLabel}
+            src={data.src}
+            isRounded
+            ratio="3/4"
+            width="large"
+            addClassNames="mb-8"
+          />
+        ))}
+      </ImageCarousel>
+      <ImageCarousel>
+        {mockData.map((data, index) => (
+          <ImageComponent
+            key={`bottom-carousel-${index}`}
+            alt={`${data.nameLabel || data.title}の画像`}
+            description={data.description || "シェフ"}
+            descriptionAlign="center"
+            src={data.src}
+            isCircle
+            ratio="1/1"
+            width="xSmall"
+            addClassNames="mb-8"
+          />
+        ))}
+      </ImageCarousel>
+      <ImageGrid addClassNames="mb-8">
+        {mockData.slice(0, 4).map((data, index) => (
+          <ImageComponent
+            key={`grid-${index}`}
+            src={data.src}
+            isRounded
+            alt={`${data.title}の画像`}
+            width="full"
+            favNum={data.favNum}
+            ratio="1/1"
+            nameLabel={data.nameLabel}
+          />
+        ))}
+      </ImageGrid>
+      <ImageComponent
+        src="/images/sample_chef.jpg"
+        alt="シェフの画像"
+        width="full"
+        nameLabel="たけちゃんシェフ"
+        ratio="3/4"
+      />
+    </>
+  );
+};
+
+export default Home;
