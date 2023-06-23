461a5ac429cb7a730903baac2e949debad6e1753
yoko-at-home <tw107sf.ham@icloud.com>
2023-06-23 18:00:05 +0900

    add domain for external image

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   images: {
-    domains: ["ca.slack-edge.com"],
+    domains: ["ca.slack-edge.com", "images.unsplash.com"],
   },
 };
 
