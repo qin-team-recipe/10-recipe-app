2eef3df07944b6631c8d11c19ed9afe42312729c
yoko-at-home <tw107sf.ham@icloud.com>
2023-06-23 18:43:13 +0900

    create recipe page

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,5 +1,75 @@
-const Recipe = () => {
-  return <div>Recipe pageです。</div>;
+/* eslint-disable import/first */
+
+/* eslint-disable tailwindcss/classnames-order */
+/* eslint-disable tailwindcss/no-custom-classname */
+
+import Link from "next/link";
+
+import { mockDataRecipe } from "@/mock";
+
+import { FavButton } from "@/components/button";
+import { ArrowIcon, ThreeDotsIcon } from "@/components/icons";
+import { ImageComponent, ImageGrid } from "@/components/image";
+
+/* eslint-disable import/first */
+const recipeNumber = 1234;
+const followerNumber = 5678;
+
+export const generateStaticParams = () => {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+};
+// https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+
+const RecipePage = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  return (
+    <div className="relative mx-auto">
+      <button type="button" className="absolute left-3 top-3 z-10 cursor-pointer rounded-full p-1.5">
+        <ArrowIcon />
+      </button>
+      <ImageComponent image_url={mockDataRecipe[0].image_url1} alt={""} ratio={"1/1"} width={"full"} />
+      <div className="pt-4">
+        <div className="flex justify-between px-4">
+          <div>シェフのレシピ</div>
+          <div>
+            <button
+              className="-mr-0.5 -mt-px rounded p-1 outline-none hover:bg-gray focus-visible:ring-2"
+              type="button"
+              id="radix-:r0:"
+              aria-haspopup="menu"
+              aria-expanded="false"
+              data-state="closed"
+            >
+              <ThreeDotsIcon />
+            </button>
+          </div>
+        </div>
+        <div className="px-4">
+          <div>
+            吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+          </div>
+          <div className="mt-3 flex  justify-start">
+            <div className="mr-3">{recipeNumber}レシピ</div>
+            <div>{followerNumber}フォロアー</div>
+          </div>
+          <div>
+            <FavButton />
+          </div>
+        </div>
+        <div>
+          <div className="mt-3 flex justify-center text-center">
+            <div className="border-light_gray w-1/2 border-b-2">作り方</div>
+            <div className="w-1/2 border-b border-gray/20 hover:border-b-2  hover:border-gray">材料</div>
+          </div>
+        </div>
+        <ul>
+          <li>A</li>
+          <li>B</li>
+          <li>C</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-export default Recipe;
+export default RecipePage;
