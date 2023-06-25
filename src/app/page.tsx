e1760c6ffe2016d8a49277a242a731400d37ad5c
mura-0828 <127912060+mura-0828@users.noreply.github.com>
2023-06-25 16:03:23 +0900

    page.tsxの変更を削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-"use client";
-
-import { useEffect } from "react";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
-  useEffect(() => {
-    const getData = async () => {
-      const data = await fetch("http://localhost:3000/api/recipeWithIngredientInstruction", { cache: "no-store" });
-      const json = await data.json();
-      console.log(json);
-    };
-    getData();
-  }, []);
-
   return <div className="text-tomato">トップページ</div>;
 };
 
