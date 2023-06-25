79ef995541b2c05237b477d3002ce02d2c0bc363
chibataxile115 <chibaxile0000115@gmail.com>
2023-06-25 22:28:42 +0900

    Favページ作成「スタイルのみ実装」

---
+++ src/app/fav/_component/index.ts
@@ -0,0 +1,7 @@
+export { ChefContents } from "./ChefContents";
+export { ChefCard } from "./ChefCard";
+export { NewRecipeCard } from "./NewRecipeCard";
+export { SectionContainer } from "./SectionContainer";
+export { NewRecipeContents } from "./NewRecipeContents";
+export { FavRecipeCard } from "./FavRecipeCard";
+export { FavRecipeContents } from "./FavRecipeContents";
