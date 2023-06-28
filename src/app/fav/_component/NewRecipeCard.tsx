35accd4735dc66d872dfde19eaba2de83bf259b6
chibataxile115 <chibaxile0000115@gmail.com>
2023-06-28 22:36:00 +0900

    削除

--- src/app/fav/_component/NewRecipeCard.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export const NewRecipeCard = () => {
-  return (
-    <Link className="contents" href="/recipe">
-      <div className="aspect-square bg-tomato"></div>
-    </Link>
-  );
-};
