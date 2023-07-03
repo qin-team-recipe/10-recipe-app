5bdf1adff6c32f1be14fe0f8cd2da29d4102ec02
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-07-03 23:54:58 +0900

    Create TabsLink component

--- src/components/tab/TabsLink.tsx
+++ src/components/tab/TabsLink.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import { UrlObject } from "url";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-import { TabWrapper } from "@/components/tab/TabWrapper";
 
 export const generateStaticParams = () => {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
@@ -15,37 +14,31 @@ type TabsLinkProps = {
     href: string;
   }[];
 };
-const tabMock = [
-  {
-    label: "レシピ",
-    href: `/chef/1`,
-  },
-  {
-    label: "リンク",
-    href: "/chef/1/link",
-  },
-];
-export const TabsLink: React.FC<TabsLinkProps> = () => {
+
+export const TabsLink: React.FC<TabsLinkProps> = ({ tabMock }) => {
   const pathname = usePathname();
 
   return (
-    <TabWrapper>
-      {tabMock.map((tab) => {
-        const isActive = pathname.endsWith(tab.href);
-
-        return (
-          <Link
-            className={`w-full border-b-2 p-2 text-center outline-none ${
-              isActive ? " border-gray font-semibold" : "border-lightGray"
-            } `}
-            // TODO: url検証、生成ロジック
-            href={tab.href as unknown as URL}
-            key={tab.label}
-          >
-            {tab.label}
-          </Link>
-        );
-      })}
-    </TabWrapper>
+    <div className="flex items-center justify-center py-12">
+      <div className="flex w-full max-w-md flex-col gap-y-2">
+        <div className="flex items-center justify-between p-1">
+          {tabMock.map((tab) => {
+            const isActive = pathname.endsWith(tab.href);
+            return (
+              <Link
+                className={`w-full border-b-2 p-2 text-center outline-none ${
+                  isActive ? " border-gray font-semibold" : "border-lightGray"
+                } `}
+                // TODO: url検証、生成ロジック
+                href={tab.href as unknown as UrlObject}
+                key={tab.label}
+              >
+                {tab.label}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 };
