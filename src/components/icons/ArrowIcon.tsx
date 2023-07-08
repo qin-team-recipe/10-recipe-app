72a679e19d5460f98a71f525f443252f3b5ba046
yoko-at-home <tw107sf.ham@icloud.com>
2023-07-08 09:48:05 +0900

    fix browser warnings (svgs)

--- src/components/icons/ArrowIcon.tsx
+++ src/components/icons/ArrowIcon.tsx
@@ -7,9 +7,9 @@ export const ArrowIcon = () => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="h-6 w-6"
     >
       <path d="M5 12l14 0"></path>
