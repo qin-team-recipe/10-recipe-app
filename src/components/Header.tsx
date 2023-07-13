1d568de43b2f77134ea47bf5f1a683fa1c3c4d9f
chibataxile115 <chibaxile0000115@gmail.com>
2023-07-13 10:51:32 +0900

    export constに変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,23 +10,22 @@ type Props = {
   isMenuIcon?: boolean;
 };
 
-const HeaderLayout: React.FC<Props> = (props) => {
-  const sizeClass = cc(["flex items-center justify-between border-b border-lightGray p-4"]);
+export const Header: React.FC<Props> = (props) => {
   const titleClass = cc([
+    "w-full px-2 text-large font-bold",
     {
       "text-left": props.position === "left",
       "text-center": props.position === "center",
     },
   ]);
   return (
-    <div className={cc([titleClass, sizeClass, props.addClassNames])}>
+    <div className={cc(["flex items-center justify-between border-b border-lightGray p-4", props.addClassNames])}>
       <div className="w-6">
         {props.isBrowserBackIcon && <IconArrowLeft />}
         {props.isMenuIcon && <IconMenu />}
       </div>
-      <h1 className="w-full px-2 text-large font-bold">{props.title}</h1>
+      <h1 className={titleClass}>{props.title}</h1>
       <div className="w-6">{props.isUserIcon && <IconUserCircle className="w-6" />}</div>
     </div>
   );
 };
-export default HeaderLayout;
