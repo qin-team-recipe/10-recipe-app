b64f04c0556e835827fe29a11ff13751374f48ba
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-07-20 21:38:34 +0900

    Header(仮)を実装

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
+import { Icon } from "@/components/icon/Icon";
+
 const SettingsPage = () => {
-  return <div>SettingsPage</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="flex items-center border-b border-lightGray p-1.5">
+        <div className="">
+          <Link href="/fav">
+            <Icon type="ArrowLeft" color="black" />
+          </Link>
+        </div>
+        <div className="py-2">設定</div>
+      </div>
+      SettingsPage
+    </div>
+  );
 };
 
 export default SettingsPage;
