6862f91abf5c0341db47c4f3d9d7f3c5ad0a8adb
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-07-22 13:52:28 +0900

    各リンクを配置（work in progress

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,20 +1,62 @@
+import { NextPage } from "next";
 import Link from "next/link";
 
 import { Icon } from "@/components/icon/Icon";
 
-const SettingsPage = () => {
+const LinkTitle = [
+  // TODO：プログラム的に外部リンクを判定させたい気持ちも...
+  // 運営会社・お問い合わせの外部リンクは後日提供
+  { title: "利用規約", url: "settings/tos", isExternalLink: false },
+  { title: "プライバシーポリシー", url: "settings/privacy", isExternalLink: false },
+  { title: "運営会社", url: "https://xxxx.com", isExternalLink: true },
+  { title: "お問い合わせ", url: "https://xxxx.com/contact", isExternalLink: true },
+];
+
+const SettingsPage: NextPage = () => {
   return (
     <div className="min-h-screen">
-      <div className="flex items-center border-b border-lightGray p-1.5">
-        <div className="">
-          {/* ↓pathname この書き方でないとWarningになる。自分の環境だけ？💦 */}
-          <Link href={`pathname: /fav`}>
+      <div className="flex border-b border-lightGray p-1.5">
+        <div className="h-4">
+          {/* pathname:このやり方でないとBuildエラーになる... */}
+          <Link href={{ pathname: "/fav" }}>
             <Icon type="ArrowLeft" color="black" />
           </Link>
         </div>
-        <div className="py-2">設定</div>
+        <div className="py-2 leading-4">設定</div>
       </div>
-      SettingsPage
+      <div className="flex flex-col justify-between gap-3 p-4">
+        <div>
+          <p className="pb-4 text-large font-semibold">利用規約や問い合わせ</p>
+          <ul className="flex flex-col gap-4">
+            {LinkTitle.map((l) => {
+              return (
+                <li key={l.title} className="flex justify-between">
+                  {l.title}
+                  {/* pathname:このやり方でないとBuildエラーになる... */}
+                  <Link href={{ pathname: l.url }} target={l.isExternalLink ? `_blank` : `_self`}>
+                    <Icon size="medium" type={l.isExternalLink ? `ExternalLink` : `ChevronRight`} color="gray" />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div>
+          <p className="pb-4 pt-8 text-large font-semibold ">アカウントの操作</p>
+          <ul className="flex flex-col gap-4">
+            <li>
+              <Link href="/settings/logout">ログアウト</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <p className="pb-4  pt-8 text-large font-semibold ">取り消しができない操作</p>
+          <ul className="flex flex-col gap-4">
+            {/* TODO：確認モーダルを出して delete と打ったら削除するロジック */}
+            <li>退会する</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
