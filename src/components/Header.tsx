720d7e3ecb834aea096878cf35a47fbfc96ccf9a
mura-0828 <127912060+mura-0828@users.noreply.github.com>
2023-07-30 07:13:09 +0900

    サーチバーを見た目だけ作成 (#91)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 
 import { IconArrowLeft, IconMenu, IconUserCircle } from "@tabler/icons-react";
 import cc from "classcat";
+import { Search } from "tabler-icons-react";
 
 type Props = {
-  title: string;
+  title?: string;
   addClassNames?: string;
   position?: "left" | "center";
   isUserIcon?: boolean;
   browserBackHref?: UrlObject;
   isMenuIcon?: boolean;
+  isSearchBar?: boolean;
 };
 
 export const Header: React.FC<Props> = (props) => {
@@ -35,7 +37,18 @@ export const Header: React.FC<Props> = (props) => {
           </Link>
         )}
       </div>
-      <h1 className={titleClass}>{props.title}</h1>
+      {props.isSearchBar && (
+        <label className="flex w-full items-center rounded-lg bg-lightGray p-2" htmlFor="searchInput">
+          <Search className="w-6 text-gray" />
+          <input
+            type="text"
+            id="searchInput"
+            placeholder="シェフやレシピを検索"
+            className="ml-2 flex-1 bg-lightGray text-gray focus:outline-none"
+          />
+        </label>
+      )}
+      {props.title && <h1 className={titleClass}>{props.title}</h1>}
       <div className="w-6">
         {props.isUserIcon && (
           <Link href={"/fav/my"}>
