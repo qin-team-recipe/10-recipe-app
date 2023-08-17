2955a916d84d924215796b5b613dcd6f867e3964
Kohno Taiyo <59274850+KohnoTaiyo@users.noreply.github.com>
2023-08-17 22:24:23 +0900

    エラーコンポーネントの作成 (#116)
    * error test
    
    * fix: test
    
    * fix: codeorner
    
    * fix: delet test fetch

--- src/app/sample/error.tsx
+++ src/app/sample/error.tsx
@@ -2,15 +2,24 @@
 
 import { useEffect } from "react";
 
-// TODO: 仮のエラーページなので他でちゃんとしたものに置き換える
-const Error = ({ error }: { error: Error }) => {
+import { ErrorComponent } from "@/components/ErrorComponent";
+
+// NOTE: 何度も同じ物を書く必要があるのでpropsの型は例外で外で定義しない
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
   useEffect(() => {
     // 本来はホスティング先にLog出力するなどする
     // eslint-disable-next-line no-console
     console.error(error);
   }, [error]);
 
-  return <p>エラーページです。</p>;
+  return (
+    <ErrorComponent
+      title="データの取得に失敗しました。"
+      text="時間を置いて再度試していただくか再読み込みボタンで読み込み直しを行なってください。"
+      reFetchText="再読み込み"
+      reFetchFunc={reset}
+    />
+  );
 };
 
 export default Error;
