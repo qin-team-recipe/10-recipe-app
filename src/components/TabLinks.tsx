2f1864032dd70d17cfcdd18ccaec8257fad8a3b4
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-08-19 21:53:25 +0900

    leadingを設定

--- src/components/TabLinks.tsx
+++ src/components/TabLinks.tsx
@@ -14,13 +14,13 @@ type TabLinksProps = {
 
 export const TabLinks: React.FC<TabLinksProps> = ({ tabs }) => {
   return (
-    <div className="flex items-center justify-center pt-6">
+    <div className="mt-3 flex items-center justify-center">
       <div className="flex w-full max-w-md flex-col">
         <div className="flex items-center justify-between">
           {tabs.map((tab) => {
             // eslint-disable-next-line tailwindcss/no-custom-classname
             const tabClass = cc([
-              "text-md w-full flex-1 border-b-2 py-2 text-center ",
+              "text-md w-full flex-1 border-b-2 py-2 text-center leading-[17px]",
               {
                 "border-black font-semibold border-b-2": tab.isActive,
                 "border-lightGray": !tab.isActive,
