4cf903288ab692511526bac9f1f0def0106ea1dc
yoko <yoko_iwasakijp@yahoo.co.jp>
2023-08-21 11:23:01 +0900

    Remove redirect setting (next.config) (#119)

--- next.config.js
+++ next.config.js
@@ -6,16 +6,6 @@ const nextConfig = {
   images: {
     domains: ["ca.slack-edge.com", "images.unsplash.com", "ttkwelkbrvhyabgfiaia.supabase.co"],
   },
-    // 中間発表での表示を目的としてリダイレクトを設定
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/search",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
