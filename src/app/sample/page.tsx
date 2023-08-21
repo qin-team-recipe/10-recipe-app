7a8939808e24920f587b63f8d564d53447e47981
KohnoTaiyo <59274850+KohnoTaiyo@users.noreply.github.com>
2023-08-21 23:07:13 +0900

    fix: cache

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -9,24 +9,25 @@ const SamplePage = async () => {
   // サーバーコンポーネントの場合
   // revalidateは何秒キャッシュされたデータを使うかの設定
   // 開発中はキャッシュが邪魔になるので1秒に設定
-  // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/chef`, { next: { revalidate: 1 } });
-  // const chefs: User[] = await res.json();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/chef`, { cache: "no-store" });
+  const chefs: User[] = await res.json();
 
   return (
     <>
       <p className="mb-2 pl-4 text-large font-bold">GETしたデータをmapで表示させる場合</p>
       <ImageCarousel>
-        {/* {chefs.map((chef) => (
-          <ImageComponent
-            key={chef.id}
-            src={chef.image_url}
-            alt={`${chef.name}の画像`}
-            nameLabel={chef.name}
-            ratio="3/4"
-            width="large"
-            addClassNames="mb-4"
-          />
-        ))} */}
+        {chefs.length &&
+          chefs.map((chef) => (
+            <ImageComponent
+              key={chef.id}
+              src={chef.image_url}
+              alt={`${chef.name}の画像`}
+              nameLabel={chef.name}
+              ratio="3/4"
+              width="large"
+              addClassNames="mb-4"
+            />
+          ))}
       </ImageCarousel>
       <hr className="mb-4" />
       <div className="m-4 rounded-md border border-black p-2">
