a90264a4da50044be821c6d160dec10dd46f2a42
ArtAco <63841250+artaco@users.noreply.github.com>
2023-08-21 21:34:40 +0900

    Feature#39 aco (#45)
    * 買い物リストページの表示部分のみの作成（ログイン・ログアウト）の大枠は完了
    
    * yarnをv1.22.17に変更したことに伴う不要ファイルの削除とyarn.lockの更新
    
    * レビュー対応：フォントカラーの指定にtailwind.config.jsを利用
    
    * レビュー対応：map関数を利用してリストを表示、
    追加対応：フォントサイズとwidthの指定にtailwind.config.jsを利用
    
    * []で記述しているCSSを全て純正のtailwindで置き換え
    
    * imgタグをImageタグに変更
    
    * yarn.lockをdevelopからコピペ
    
    * アイコンコンポーネントを利用した表示に置き換え
    
    * rootのmockに移動
    
    * 買い物リストコンポーネント化

---
+++ src/components/list/ListShoppingItem.tsx
@@ -0,0 +1,28 @@
+import { Icon } from "@/components/icon/Icon";
+
+type ListShoppingItemProps = {
+  id: number;
+  name: string;
+  is_checked: boolean;
+  children?: React.ReactNode;
+};
+
+export const ListShoppingItem: React.FC<ListShoppingItemProps> = (props) => {
+  return (
+    <div key={props.id} className="flex justify-between border-b border-solid border-lightGray px-3 py-2">
+      <div className="flex">
+        <button className="p-1">
+          {props.is_checked ? (
+            <Icon type="CircleCheck" color="white" fillColor="lightGray" />
+          ) : (
+            <Icon type="Circle" color="tomato" />
+          )}
+        </button>
+        <p className={props.is_checked ? "p-1 text-lightGray" : "p-1"}>{props.name}</p>
+      </div>
+      <div className="cursor-pointer p-1">
+        <Icon type="DotsVertical" />
+      </div>
+    </div>
+  );
+};
