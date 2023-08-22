6e1439335e0819794833a87842bcf535f9ce79c2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-08-22 22:48:56 +0900

    fix css

--- src/components/TabLinks.tsx
+++ src/components/TabLinks.tsx
@@ -14,26 +14,21 @@ type TabLinksProps = {
 
 export const TabLinks: React.FC<TabLinksProps> = ({ tabs }) => {
   return (
-    <div className="mt-3 flex items-center justify-center">
-      <div className="flex w-full max-w-md flex-col">
-        <div className="flex items-center justify-between">
-          {tabs.map((tab) => {
-            // eslint-disable-next-line tailwindcss/no-custom-classname
-            const tabClass = cc([
-              "text-md w-full flex-1 border-b-2 py-2 text-center leading-[17px]",
-              {
-                "border-black font-semibold border-b-2": tab.isActive,
-                "border-lightGray": !tab.isActive,
-              },
-            ]);
-            return (
-              <Link href={{ pathname: tab.href }} key={tab.label} className={tabClass}>
-                {tab.label}
-              </Link>
-            );
-          })}
-        </div>
-      </div>
+    <div className="flex">
+      {tabs.map((tab) => {
+        const tabClass = cc([
+          "w-full flex-1 border-b-2 py-2 text-center text-medium",
+          {
+            "border-black font-semibold border-b-2": tab.isActive,
+            "border-lightGray": !tab.isActive,
+          },
+        ]);
+        return (
+          <Link href={{ pathname: tab.href }} key={tab.label} className={tabClass}>
+            {tab.label}
+          </Link>
+        );
+      })}
     </div>
   );
 };
