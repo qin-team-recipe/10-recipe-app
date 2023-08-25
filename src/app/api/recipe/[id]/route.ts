62ffab4f9e33b7df1ecbdb33b2144cf0f4694046
mura-0828 <workmura@icloud.com>
2023-08-25 18:56:47 +0900

    trycatchを削除、単一取得をrecipe/[id]へ、delete,patchの順番を変更

--- src/app/api/recipe/[id]/route.ts
+++ src/app/api/recipe/[id]/route.ts
@@ -4,18 +4,17 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
-  try {
-    const data = await prisma.recipe.delete({
-      where: {
-        id: params.id,
-      },
-    });
-    return NextResponse.json(data);
-  } catch (e: any) {
-    return NextResponse.json({ error: e.message, message: "エラーが発生しました。" });
-  }
+export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  const data = await prisma.recipe.findUnique({
+    where: {
+      id: id as string,
+    },
+  });
+  return NextResponse.json(data);
 }
+
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
   const { title, description, image_url, status, instruction } = await request.json();
   const body = {
@@ -25,17 +24,22 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     status,
     instruction,
   };
-  try {
-    const data = await prisma.recipe.update({
-      where: {
-        id: params.id,
-      },
-      data: {
-        ...body,
-      },
-    });
-    return NextResponse.json(data);
-  } catch (e: any) {
-    return NextResponse.json({ error: e.message, message: "エラーが発生しました。" });
-  }
+  const data = await prisma.recipe.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      ...body,
+    },
+  });
+  return NextResponse.json(data);
+}
+
+export async function DELETE({ params }: { params: { id: string } }) {
+  const data = await prisma.recipe.delete({
+    where: {
+      id: params.id,
+    },
+  });
+  return NextResponse.json(data);
 }
--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -4,22 +4,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id");
-
-  if (id) {
-    const res = await prisma.recipe.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    return NextResponse.json(res);
-  } else {
-    const res = await prisma.recipe.findMany();
-    return NextResponse.json(res);
-  }
+export async function GET() {
+  const res = await prisma.recipe.findMany();
+  return NextResponse.json(res);
 }
+
 export async function POST(req: NextRequest) {
   const { title, description, image_url, status, instruction } = await req.json();
   const body = {
@@ -29,15 +18,11 @@ export async function POST(req: NextRequest) {
     status,
     instruction,
   };
-  try {
-    const data = await prisma.recipe.create({
-      data: {
-        ...body,
-        user_id: "56028e14-9455-41b5-b6b1-eef299b7ef72",
-      },
-    });
-    return NextResponse.json(data);
-  } catch (e: any) {
-    return NextResponse.json({ error: e.message, message: "エラーが発生しました。" });
-  }
+  const data = await prisma.recipe.create({
+    data: {
+      ...body,
+      user_id: "56028e14-9455-41b5-b6b1-eef299b7ef72",
+    },
+  });
+  return NextResponse.json(data);
 }
