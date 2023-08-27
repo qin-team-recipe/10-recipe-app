9de085791565650098471af278d850d4283d127e
Yohji_Aono <75562041+perio29@users.noreply.github.com>
2023-08-27 15:38:03 +0900

    #101 React Hook FormとZodでフォームを実装 (#143)
    * resolve conflicts
    
    * #101 Add deletion feature
    
    * delete .idea
    
    ---------
    
    Co-authored-by: 青野耀慈 <aono@MacBook-Pro-5.local>

---
+++ src/app/recipe/new/_components/AddButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+import { Icon } from "@/components/Icon/Icon";
+
+type Props = {
+  title: string;
+  onClick?: () => void;
+};
+
+export const AddButton: React.FC<Props> = (props) => {
+  return (
+    <button type="button" onClick={props.onClick} className="ml-4 mt-2 flex items-center gap-1">
+      <Icon type="Plus" color="tomato" />
+      <p className="text-tomato">{props.title}</p>
+    </button>
+  );
+};
