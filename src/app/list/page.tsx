a32a2f81664220f8cd2dfa68dd0fcb9bccf03df8
ArtAco <63841250+artaco@users.noreply.github.com>
2023-09-04 19:24:28 +0900

    買い物リストページにHeaderコンポーネントを適用。セッション状態による表示切り替え処理も導入 (#153)
    * 買い物リストページにHeaderコンポーネントを適用。セッション状態による表示切り替え処理も導入
    
    * ヘッダーコンポーネントのisMenuIconフラグの削除とログインコンポーネントの導入

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,18 +1,20 @@
-import { NextPage } from "next";
-import Image from "next/image";
-
+import { serverComponentSupabase } from "@/lib/serverComponentSupabase";
 import { MockMyList, MockRecipeList } from "@/mock";
 
+import { Header } from "@/components/Header/Header";
 import { ListShoppingHeader } from "@/components/list/ListShoppingHeader";
 import { ListShoppingItem } from "@/components/list/ListShoppingItem";
+import { Login } from "@/components/Login/Login";
 
-const Page: NextPage = () => {
-  const isLogin = true;
+const ListPage = async () => {
+  const {
+    data: { session },
+  } = await serverComponentSupabase.auth.getSession();
 
   return (
     <>
-      <p className="w-full border-b border-lightGray p-2 text-center font-bold">買い物リスト</p>
-      {isLogin ? (
+      <Header position="center" title="買い物リスト" />
+      {session ? (
         <div>
           <div>
             <ListShoppingHeader title="じぶんメモ" />
@@ -32,23 +34,10 @@ const Page: NextPage = () => {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col items-center">
-          <Image className="w-64" src="/images/sample_list.png" width="256" height="256" alt="" />
-          <p className="py-2 text-center font-bold">ログインをお願いします</p>
-          <p className="text-center text-medium">こちらの機能を利用するにはログインが必要です</p>
-          <div className="flex w-96 justify-between py-4">
-            <button className="w-44 rounded-xl  bg-blue p-2.5 font-bold text-white hover:opacity-80">
-              Google ログイン
-            </button>
-            <button className="w-44 rounded-xl  bg-black p-2.5 font-bold text-white hover:opacity-80">
-              Apple ログイン
-            </button>
-          </div>
-          <p className="text-center text-small">↑ cookiesを使ってログインっぽい挙動にしてます ↑</p>
-        </div>
+        <Login imageType="shopping" />
       )}
     </>
   );
 };
 
-export default Page;
+export default ListPage;
