6e893a34927d089d1bb9a86b4a551fbfbf735c36
artaco <63841250+artaco@users.noreply.github.com>
2023-09-04 16:21:44 +0900

    ヘッダーコンポーネントのisMenuIconフラグの削除とログインコンポーネントの導入

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 import { serverComponentSupabase } from "@/lib/serverComponentSupabase";
 import { MockMyList, MockRecipeList } from "@/mock";
 
 import { Header } from "@/components/Header/Header";
 import { ListShoppingHeader } from "@/components/list/ListShoppingHeader";
 import { ListShoppingItem } from "@/components/list/ListShoppingItem";
+import { Login } from "@/components/Login/Login";
 
 const ListPage = async () => {
   const {
@@ -14,7 +13,7 @@ const ListPage = async () => {
 
   return (
     <>
-      <Header position="center" title="買い物リスト" isMenuIcon={!!session} />
+      <Header position="center" title="買い物リスト" />
       {session ? (
         <div>
           <div>
@@ -35,20 +34,7 @@ const ListPage = async () => {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col items-center">
-          <Image className="w-64" src="/images/sample_list.png" width="256" height="256" alt="" />
-          <p className="py-2 text-center font-bold">ログインをお願いします</p>
-          <p className="text-center text-medium">こちらの機能を利用するにはログインが必要です</p>
-          <div className="flex w-96 justify-between py-4">
-            <button className="w-44 rounded-xl  bg-blue p-2.5 font-bold text-white hover:opacity-80">
-              Google ログイン
-            </button>
-            <button className="w-44 rounded-xl  bg-black p-2.5 font-bold text-white hover:opacity-80">
-              Apple ログイン
-            </button>
-          </div>
-          <p className="text-center text-small">↑ cookiesを使ってログインっぽい挙動にしてます ↑</p>
-        </div>
+        <Login imageType="shopping" />
       )}
     </>
   );
