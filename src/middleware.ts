578b8962dc8e974ea055466ddd186d556acf04e2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-09-04 13:51:09 +0900

    middlewareでurlを取得できるらしい

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,20 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "./types/schema";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
+  // このやり方でURLが取得できるらしい
+  //https://github.com/vercel/next.js/issues/43704
+  // const requestHeaders = new Headers(req.headers);
+  // Store current request pathname in a custom header
+  // requestHeaders.set("x-pathname", req.nextUrl.pathname);
+  const res = NextResponse
+    .next
+    // {
+    // request: {
+    //   // Apply new request headers
+    //   headers: requestHeaders,
+    // },
+    // }
+    ();
   const supabase = createMiddlewareClient<Database>({ req, res });
   await supabase.auth.getSession();
   return res;
