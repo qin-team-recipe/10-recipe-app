7c533028759a3da4f6d994fc0f9bff47bfb3ef64
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2023-09-16 20:02:33 +0900

    revert changes

--- src/app/chef/[id]/_component/TopBarLinks.tsx
+++
@@ -1,3 +0,0 @@
-export const TopBarLinks: React.FC = () => {
-  return <div></div>;
-};
--- src/app/chef/[id]/layout.tsx
+++ src/app/chef/[id]/layout.tsx
@@ -1,6 +1,6 @@
 import { ImageComponent } from "@/components/Image";
 import { type Chef } from "@/app/api/chef/[id]/route";
-import { TopBarEdit } from "@/app/chef/[id]/_component/TopBarEdit";
+import { TopBar } from "@/app/chef/[id]/_component/TopBar";
 
 import { ChefFollowButton } from "./_component/ChefFollowButton";
 
@@ -12,7 +12,7 @@ const ChefLayout = async ({ children, params }: { children: React.ReactNode; par
   return (
     <div>
       <div className="space-y-2 p-4">
-        <TopBarEdit />
+        <TopBar />
         <div className="flex items-center justify-between">
           <div className="mr-2">
             <h3 className="text-title font-bold">{chef.name}</h3>
